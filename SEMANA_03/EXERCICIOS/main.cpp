033fdf573a7def9d801b8ec44c33cb10c7ad25c8
IaagoTavares <99209029+IaagoTavares@users.noreply.github.com>
2022-11-02 18:20:17 -0300

    Atualização

--- SEMANA_03/EXERCICIOS/main.cpp
+++ SEMANA_03/EXERCICIOS/main.cpp
@@ -40,11 +40,19 @@ void setup() {
 
 void loop() {
   
-
-  // pintaLed();
-
+  // 1 - Faça uma função que, a cada novo número medido,
+  // faça a contagem binária nos leds, pausadamente, de 0 até o número medido.
+  // 2 - Acrescente "toque" usando um Buzzer que corresponda a uma nota diferente
+  // para cada número. Ao contar de 0 até o número, o buzzer irá tocando todas as notas correspondentes.
+  // ler os dados de luz de um sensor LDR
+  pintaLed();
   tocaMusica();
+
+  // 3 - Acrescente um "push button", tal que, ao ser pressionado, armazene o valor medido em um vetor.
   gravador();
+
+
+  // 4 - Acrescente um SEGUNDO push button que, ao ser pressionado, toque todos os tons correspondentes ao número armazenado.
   reprodutor();
 
 }
